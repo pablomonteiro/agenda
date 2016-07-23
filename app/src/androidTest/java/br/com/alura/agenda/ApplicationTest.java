d694c7ca2943fc39caa5ecc48c287c33e4915c27
Pablo Monteiro <pablo@lamborghini.local>
2016-07-23 15:09:14 -0300

    Adicionando configuração para inclusão de testes de instrumental utilizando o Espresso.

--- app/src/androidTest/java/br/com/alura/agenda/ApplicationTest.java
+++ app/src/androidTest/java/br/com/alura/agenda/ApplicationTest.java
@@ -1,13 +1,29 @@
 package br.com.alura.agenda;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.test.ApplicationTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(JUnit4.class)
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<FormularioActivity> activity = new ActivityTestRule<>(FormularioActivity.class);
+
+    @Test
+    public void firstTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.formulario_nome)).check(ViewAssertions.matches(ViewMatchers.withText("")));
     }
+
 }
